014b3732f70b46f607f470cc04dcdc2b2839f416
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-06-19 09:41:07 -0700

    Register section

--- src/App.vue
+++ src/App.vue
@@ -70,6 +70,57 @@ export default {
   }
 }
 
+button,
+.router-button {
+  transition: 500ms ease all;
+  cursor: pointer;
+  margin-top: 24px;
+  padding: 12px 24px;
+  background-color: #303030;
+  color: #fff;
+  border-radius: 20px;
+  border: none;
+  text-transform: uppercase;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background-color: rgba(48,48,48,0.7);
+  }
+}
+
+.button-ghost {
+  color: #000;
+  padding: 0;
+  border-radius: 0;
+  margin-top: 50px;
+  font-size: 15px;
+  font-weight: 500;
+  background-color: transparent;
+  @media (min-width: 700px) {
+    margin-top: 0;
+    margin-left: auto;
+  }
+
+  i {
+    margin-left: 8px;
+  }
+}
+
+.button-light {
+  background-color: transparent;
+  border: 2px solid #fff;
+  color: #fff;
+}
+
+.button-inactive {
+  pointer-events: none !important;
+  cursor: none !important;
+  background-color: rgba(128, 128, 128, 0.5) !important;
+}
+
 .blog-card-wrap {
   position: relative;
   padding: 80px 16px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,16 +11,28 @@
         </div>
       </div>
     </div>
+
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a post.  Register for your free account today!</h2>
+        <router-link class="router-button" to="#">
+          Register for FireBlogs <Arrow class="arrow arrow-light"/>
+        </router-link>
+      </div>
+    </div>
+
+
     <!--<img alt="Vue logo" src="../assets/logo.png" />-->
   </div>
 </template>
 
 <script>
-import BlogPost from '../components/BlogPost.vue'
-import BlogCards from '../components/BlogCard.vue'
+import BlogPost from '../components/BlogPost.vue';
+import BlogCards from '../components/BlogCard.vue';
+import Arrow from '../assets/Icons/arrow-right-light.svg';
 export default {
   name: "Home",
-  components: { BlogPost, BlogCards },
+  components: { BlogPost, BlogCards, Arrow},
   data() {
     return {
       welcomeScreen: {
@@ -60,6 +72,43 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+
+  }
+}
+
+.updates {
+
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media(min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media(min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
 
   }
 }
