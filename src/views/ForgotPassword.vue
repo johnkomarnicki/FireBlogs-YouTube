229fe35a4cef3911ff1987785193d8959d6579d1
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-06-20 09:46:39 -0700

    Forgot password

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,13 +1,70 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+    <Modal v-if="modalActive" v-on:close-modal="closeModal"/>
+    <Loading v-if="loading" />
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset Password</h2>
+        <p>Forgot your password? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button>Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import email from "../assets/Icons/envelope-regular.svg"
+import Modal from "../components/Modal.vue";
+import Loading from "../components/Loading.vue";
 export default {
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: null,
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    }
+  },
+  components: {
+    email,
+    Modal,
+    Loading,
+},
+  methods: {
+    closeModal() {
+      this.modalActive = !this.modalActive;
+      this.email = "";
+    }
 
-}
+  } 
+
+
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
 
 </style>
