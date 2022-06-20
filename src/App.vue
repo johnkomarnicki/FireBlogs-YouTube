de8392e8353da8e6494fc9aefafefcd101f713d8
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-06-20 08:38:29 -0700

    Login page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
-      <Navigation />
+      <Navigation v-if="navigation"/>
       <router-view />
-      <Footer />
+      <Footer v-if="navigation" />
     </div>
   </div>
 </template>
@@ -15,12 +15,28 @@ export default {
   name: "app",
   components: { Navigation, Footer },
   data() {
-    return {};
+    return {
+      navigation: null,
+    };
   },
-  created() {},
+  created() {
+    this.checkRoute();
+  },
   mounted() {},
-  methods: {},
-  watch: {},
+  methods: {
+    checkRoute() {
+      if (this.$route.name === "Login" | this.$route.name === "Register" | this.$route.name === "ForgotPassword") {
+        this.navigation = false;
+        return;
+      }
+      this.navigation = true;
+    }
+  },
+  watch: {
+    $route() {
+      this.checkRoute();
+    }
+  },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Blogs from "../views/Blogs.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
 
 Vue.use(VueRouter);
 
@@ -10,11 +13,43 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta : {
+      title: 'Home',
+
+    },
   },
   {
     path: "/blogs",
     name: "Blogs",
     component: Blogs,
+    meta : {
+      title: 'Blogs',
+      
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+    meta : {
+      title: 'Login',      
+    },
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    meta : {
+      title: 'Register',      
+    },
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: ForgotPassword,
+    meta : {
+      title: 'Forgot Password',
+    },
   },
 ];
 
@@ -23,5 +58,10 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | FireBlog`;
+  next();
+})
 
 export default router;
