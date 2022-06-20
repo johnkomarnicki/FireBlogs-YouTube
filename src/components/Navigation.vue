1f4c6b6f22838912f99f032659c6b4d6fc388be3
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-06-20 09:50:21 -0700

    Routes

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,7 +9,7 @@
         <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
         <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
         <router-link class="link" to="#">Create Post</router-link>
-        <router-link class="link" to="#">Login/Register</router-link>
+        <router-link class="link" :to="{ name: 'Login' }">Login/Register</router-link>
       </ul>
     </div>
   </nav>
@@ -19,7 +19,7 @@
       <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
       <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
       <router-link class="link" to="#">Create Post</router-link>
-      <router-link class="link" to="#">Login/Register</router-link>
+      <router-link class="link" :to="{ name: 'Login' }">Login/Register</router-link>
     </ul>
   </transition>
 </header>
